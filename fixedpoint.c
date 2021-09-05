2b119f23323f3c124b6e019fbcdbb2d11339d70e
jchen324 <jchen324@jh.edu>
2021-09-06 04:02:05 +0800

    undo commenting out

--- fixedpoint.c
+++ fixedpoint.c
@@ -29,55 +29,53 @@ Fixedpoint fixedpoint_create2(uint64_t whole, uint64_t frac) {
 
 Fixedpoint fixedpoint_create_from_hex(const char *hex) {
   // TODO: implement
-//   Fixedpoint val;
+  Fixedpoint val;
 
-//   if (strcmp(&hex[0], "-") ==  0)
-//   {
-//     val.tag = "VN";  // VN for valid/negative
-//   } else {
-//     val.tag = "VNN";
-//   }
+  if (strcmp(&hex[0], "-") ==  0)
+  {
+    val.tag = "VN";  // VN for valid/negative
+  } else {
+    val.tag = "VNN";
+  }
   
-//   const char *pos = strchr(hex, '.');  // search for '.' in hex
-//   char *whole, *frac;
-//   int len = strlen(hex);
-//   int index;
-//   if (pos)  // "." found
-//   {
-//     index = pos - hex;
-//     if (strcmp(val.tag, "VN") == 0)  // if valid/negative
-//     {
-//       whole = (char *)malloc((index - 1) * sizeof(char));
-//       frac = (char *)malloc(len - index + 1);
-//       memcpy(whole, &hex[1], index - 1);
-//       memcpy(frac, pos, len - index + 1);
-//     } else {
-//       whole = (char *)malloc(index * sizeof(char));
-//       frac = (char *)malloc(len - index + 1);
-//       memcpy(whole, &hex[0], index);
-//       memcpy(frac, pos + 1, len - index + 1);
-//     }
-//   } else {
-//     if (strcmp(val.tag, "VN") == 0)
-//     {
-//       char *whole = (char *)malloc(len - 1);
-//       memcpy(whole, &hex[1], len - 1);
-//     } else {
-//       char *whole = (char *)malloc(len);
-//       memcpy(whole, &hex[1], len);
-//     }
-//   }
+  const char *pos = strchr(hex, '.');  // search for '.' in hex
+  char *whole, *frac;
+  int len = strlen(hex);
+  int index;
+  if (pos)  // "." found
+  {
+    index = pos - hex;
+    if (strcmp(val.tag, "VN") == 0)  // if valid/negative
+    {
+      whole = (char *)malloc((index - 1) * sizeof(char));
+      frac = (char *)malloc(len - index + 1);
+      memcpy(whole, &hex[1], index - 1);
+      memcpy(frac, pos, len - index + 1);
+    } else {
+      whole = (char *)malloc(index * sizeof(char));
+      frac = (char *)malloc(len - index + 1);
+      memcpy(whole, &hex[0], index);
+      memcpy(frac, pos + 1, len - index + 1);
+    }
+  } else {
+    if (strcmp(val.tag, "VN") == 0)
+    {
+      char *whole = (char *)malloc(len - 1);
+      memcpy(whole, &hex[1], len - 1);
+    } else {
+      char *whole = (char *)malloc(len);
+      memcpy(whole, &hex[1], len);
+    }
+  }
   
-//   char *endptr;
-//   val.whole = strtoul(whole, &endptr, 16);
-//   uint64_t literal = strtoul(frac, &endptr, 16);
-//   int shifts = (16 - strlen(frac)) * 4;
-//   val.frac = literal << shifts;
-//   free(whole);
-//   free(frac);
-//   return val;
-  assert(0);
-  return DUMMY;
+  char *endptr;
+  val.whole = strtoul(whole, &endptr, 16);
+  uint64_t literal = strtoul(frac, &endptr, 16);
+  int shifts = (16 - strlen(frac)) * 4;
+  val.frac = literal << shifts;
+  free(whole);
+  free(frac);
+  return val;
 }
 
 uint64_t fixedpoint_whole_part(Fixedpoint val) {
@@ -143,10 +141,8 @@ int fixedpoint_is_err(Fixedpoint val) {
 }
 
 int fixedpoint_is_neg(Fixedpoint val) {
-  // if (val.tag == "VN") return 1;
-  // else return 0;
-  assert(0);
-  return 0;
+  if (val.tag == "VN") return 1;
+  else return 0;
 }
 
 int fixedpoint_is_overflow_neg(Fixedpoint val) {
