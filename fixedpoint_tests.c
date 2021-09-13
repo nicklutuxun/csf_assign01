aa61206e8c21afc53ff6ec4604ed7d409494662b
Nick Lu <tlu32@jhu.edu>
2021-09-13 15:54:20 -0400

    Added more test cases

--- fixedpoint_tests.c
+++ fixedpoint_tests.c
@@ -78,8 +78,8 @@ TestObjs *setup(void) {
   objs->large1 = fixedpoint_create2(0x4b19efceaUL, 0xec9a1e2418UL);
   objs->large2 = fixedpoint_create2(0xfcbf3d5UL, 0x4d1a23c24fafUL);
   objs->max = fixedpoint_create2(0xffffffffffffffffUL, 0xffffffffffffffffUL);
-  objs->neg_1 = fixedpoint_create(-1UL);
-  objs->neg_one_eighth = fixedpoint_create2(0UL, 0x2000000000000000UL);
+  objs->neg_1 = fixedpoint_create_from_hex("-1");
+  objs->neg_one_eighth = fixedpoint_create_from_hex("-0.2");
   objs->min = fixedpoint_create_from_hex("-ffffffffffffffff.ffffffffffffffff");
 
   return objs;
@@ -307,7 +307,7 @@ void test_add(TestObjs *objs) {
   Fixedpoint lhs, rhs, sum;
 
   lhs = fixedpoint_create_from_hex("-c7252a193ae07.7a51de9ea0538c5");
-  rhs = fixedpoint_create_from_hex("ddd09079.1e6d601");
+  rhs = fixedpoint_create_from_hex("d09079.1e6d601");
   sum = fixedpoint_add(lhs, rhs);
   ASSERT(fixedpoint_is_neg(sum));
   ASSERT(0xc7252a0c31d8eUL == fixedpoint_whole_part(sum));
